a305363ecd3e606773e76b0108621d92b5834ab6
boyninja1555 <flappyfloorg@gmail.com>
2026-09-16 02:14:51 -0500

    CLI starter pack

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
-#include <wbtp.h>
+#include <stdio.h>
 
-int main(void)
+int usage(int argc, const char *argv[], const char *error)
 {
+    if (error != NULL)
+        fprintf(stderr, "%s\n", error);
+
+    fprintf(stderr, "Usage: %s [--type <get|put|request>] [--verbose] <url>\n", argv[0]);
+    return 1;
+}
+
+int main(int argc, const char *argv[])
+{
+    if (argc == 1)
+        return usage(argc, argv, "Missing required fields! '[*]' means optional, '<*>' means required. '<*>' nested inside a '[*]' means required only when optional exists.");
+
     return 0;
 }
